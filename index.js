d739671c55743793b1d62894637d04b7b89fb320
Tirumala rao <tirumalas951@gmail.com>
2025-07-03 16:33:13 +0530

    Updated config and added production.json for Render

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
 const port = process.env.PORT || 3000;
 const server=app.listen(port, () => winston.info(`Listening on port ${port}...`));
 module.exports=server;
